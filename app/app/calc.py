48d9cb7d6aab31be82adf094b7c1646dd3ce222b
salwan <mark123.mm51@gmail.com>
2024-06-18 21:41:07 +0300

    added database pkages to the dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator functions
+"""
+
+
+def add(x, y):
+    """add x and y return result."""
+    return x + y
+
+def sub(x , y):
+    return y - x
+
---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""sample test"""
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+class calctest(SimpleTestCase):
+    """tests the calc module """
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEquals(res, 11)
+
+
+    def test_sub_numbers(self):
+        res = calc.sub(10, 15)
+        self.assertEquals(res, 5)
